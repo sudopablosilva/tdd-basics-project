f3abc2d539f98fd38fbe05d1b0e9b857514dde05
Pablo Silva <pcsilva@amazon.com>
2022-11-18 20:58:59 -0300

    Adding pop method and tests; Included mysum and mysub functions to test Wallaby

--- test/stack.test.js
+++ test/stack.test.js
@@ -12,6 +12,34 @@ class Stack {
     this.top += 1;
     this.items[this.top] = value;
   }
+
+  pop(){
+    if (this.top >= 0) {
+      let valueToReturn = this.items[this.top];
+      
+      if (this.top -1 == -1) {
+        this.items = {};  
+      }else{
+        this.items[this.top] = null;
+      }
+
+      this.top -= 1;
+      
+      return valueToReturn;  
+    }else{
+      return null;
+    }
+    
+  }
+
+  mysum(a, b){
+    return a+b;
+  }
+  
+  mysub(a, b){
+    return a-b;
+  }
+
 }
 
 describe('My Stack', () => {
@@ -36,5 +64,24 @@ describe('My Stack', () => {
     expect(stack.peek).toBe('🌽');
   });
 
-  it.todo('can pop off');
+  it('can pop off', () => {
+    stack.push('🥑');
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe('🥑');
+
+    let myvalue = stack.pop();
+    expect(myvalue).toBe('🥑')
+
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({});
+
+    myvalue = stack.pop();
+    expect(myvalue).toBeNull();
+    expect(stack.top).toBe(-1);
+  });
+
+  it("Should return 2 given 1+1", () => {
+    expect(stack.mysum(1,1)).toBe(2);
+  })
+
 });
